bbf6862409278bb568307d33e95fe1f5c06acce3
ferutizi <fer.utizi@gmail.com>
2024-02-19 19:16:21 -0300

    page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import './globals.css'
+
 export default function Home (): JSX.Element {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-6">
+      <article>
+        <p>So, you want to travel to</p>
+        <h1>SPACE</h1>
+        <p>Space Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+      </article>
+      <div>
+        EXPLORE
+      </div>
     </main>
   )
 }
