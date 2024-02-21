71572a249bdcdbdeb5091ae6debb212e09d96ac6
ferutizi <fer.utizi@gmail.com>
2024-02-21 16:18:16 -0300

    text underline to currentDestination, img

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -7,12 +7,14 @@ import Subtitle from '../components/Subtitle'
 import { useState } from 'react'
 
 export default function DestinationPage (): JSX.Element {
-  const initialDestination = data.destinations.filter(e => e.name === 'Moon')
-  const [currentDestination, setCurrentDestination] = useState(initialDestination[0])
+  const initialDest = data.destinations.filter(e => e.name === 'Moon')
+  const [currentDest, setCurrentDest] = useState(initialDest[0])
+
+  const underlineText = 'underline underline-offset-8 font-normal decoration-4'
 
   const handleDestination = (name: string): void => {
     const newDestination = data.destinations.filter(e => e.name === name)
-    setCurrentDestination(newDestination[0])
+    setCurrentDest(newDestination[0])
   }
 
   return (
@@ -22,27 +24,27 @@ export default function DestinationPage (): JSX.Element {
         <Subtitle />
         <img
           className='h-48'
-          src='/assets/images/destination/image-moon.webp'
-          alt={currentDestination.name}
-          title={currentDestination.name}
+          src={`/assets/images/destination/image-${currentDest.name.toLowerCase()}.webp`}
+          alt={currentDest.name}
+          title={currentDest.name}
         />
         <nav className='flex gap-4 my-2'>
           {data.destinations.map(e =>
             <button
               onClick={() => { handleDestination(e.name) }}
               key={e.name}
-              className='alter-font tracking-widest text-sm font-thin'
+              className={`alter-font ${e.name === currentDest.name && underlineText} tracking-widest text-sm font-thin`}
             >{e.name.toUpperCase()}
             </button>
           )}
         </nav>
-        <h1 className='text-5xl'>{currentDestination.name.toUpperCase()}</h1>
-        <p className='font-thin'>{currentDestination.description}</p>
+        <h1 className='text-5xl'>{currentDest.name.toUpperCase()}</h1>
+        <p className='font-thin'>{currentDest.description}</p>
         <hr className='w-full opacity-40 my-4'></hr>
         <h3 className='alter-font text-sm font-thin tracking-widest opacity-80'>{'Avg. distance'.toUpperCase()}</h3>
-        <p className='font-bell text-2xl opacity-80'>{currentDestination.distance.toUpperCase()}</p>
+        <p className='font-bell text-2xl opacity-80'>{currentDest.distance.toUpperCase()}</p>
         <h3 className='alter-font text-sm font-thin tracking-widest opacity-80'>{'Est. travel time'.toUpperCase()}</h3>
-        <p className='font-bell text-2xl opacity-80 mb-6'>{currentDestination.travel.toUpperCase()}</p>
+        <p className='font-bell text-2xl opacity-80 mb-6'>{currentDest.travel.toUpperCase()}</p>
       </article>
     </section>
   )
