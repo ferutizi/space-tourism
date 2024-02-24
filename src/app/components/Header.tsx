0f63b3b3a40d1edbb7babcdbec785a7e3b93d12b
ferutizi <fer.utizi@gmail.com>
2024-02-24 15:07:52 -0300

    fixed some styles

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header (): JSX.Element {
       <hr className='hidden lg:inline w-99 translate-x-12 z-30 opacity-40'></hr>
       <NavMobile />
       {/* change color */}
-      <div className='hidden md:flex gap-6 text-white p-8 bg-slate-900 text-lg z-20 lg:w-3/5 over-blur-desktop lg:pl-18 lg:pr-10 lg:justify-center'>
+      <div className='hidden md:flex gap-6 text-white p-8 bg-white bg-opacity-5 text-lg z-20 lg:w-3/5 over-blur-desktop lg:pl-18 lg:pr-10 lg:justify-center'>
         <NavLink path='/' name={'Home'} index='00' />
         <NavLink path='/destination' name={'Destination'} index='01' />
         <NavLink path='/crew' name={'Crew'} index='02' />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Header from './components/Header'
 import './globals.css'
 
@@ -7,7 +9,7 @@ export default function Home (): JSX.Element {
   return (
     <section className='home h-screen'>
       <Header />
-      <main className="page-h flex flex-col items-center text-center justify-between p-6 md:justify-around lg:flex-row page-h-desktop lg:items-end lg:pb-36">
+      <main className="page-h flex flex-col items-center text-center justify-between p-6 md:justify-around lg:flex-row home-h-desktop lg:items-center">
         <article className='md:w-min lg:text-left'>
           <p className='alter-font text-lg font-thin tracking-widest lg:text-2xl'>{subtitle.toUpperCase()}</p>
           <h1 className='text-7xl my-2 md:text-10xl lg:my-4 lg:text-9xl'>SPACE</h1>
